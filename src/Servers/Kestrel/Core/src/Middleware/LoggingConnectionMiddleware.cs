7e283146f2a83d050d2a2a064a839a3e7cffcd6e
James Newton-King <james@newtonking.com>
2021-02-23 08:55:16 +1300

    WIP?

--- src/Servers/Kestrel/Core/src/Middleware/LoggingConnectionMiddleware.cs
+++ src/Servers/Kestrel/Core/src/Middleware/LoggingConnectionMiddleware.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO.Pipelines;
+using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Connections;
@@ -85,7 +86,7 @@ namespace Microsoft.AspNetCore.Server.Kestrel.Core.Internal
                 var context = await _inner.AcceptAsync(cancellationToken);
                 if (context != null)
                 {
-                    context.Transport = new LoggingDuplexPipe(context.Transport, _logger);
+                    context = new LoggingConnectionContext(context, _logger);
                 }
                 return context;
             }
@@ -94,10 +95,62 @@ namespace Microsoft.AspNetCore.Server.Kestrel.Core.Internal
             {
                 var context = await _inner.ConnectAsync(features, cancellationToken);
 
-                context.Transport = new LoggingDuplexPipe(context.Transport, _logger);
+                context = new LoggingConnectionContext(context, _logger);
 
                 return context;
             }
+
+            public override CancellationToken ConnectionClosed { get => _inner.ConnectionClosed; set => _inner.ConnectionClosed = value; }
+            public override ValueTask DisposeAsync()
+            {
+                return _inner.DisposeAsync();
+            }
+            public override EndPoint? LocalEndPoint { get => _inner.LocalEndPoint; set => _inner.LocalEndPoint = value; }
+            public override EndPoint? RemoteEndPoint { get => _inner.RemoteEndPoint; set => _inner.RemoteEndPoint = value; }
+        }
+
+        private class LoggingConnectionContext : ConnectionContext
+        {
+            private readonly ConnectionContext _inner;
+            private readonly ILogger _logger;
+            private readonly LoggingDuplexPipe _loggingPipe;
+
+            public LoggingConnectionContext(ConnectionContext inner, ILogger logger)
+            {
+                _inner = inner;
+                _logger = logger;
+
+                _loggingPipe = new LoggingDuplexPipe(_inner.Transport, _logger);
+                Transport = _loggingPipe;
+            }
+
+            public override void Abort()
+            {
+                _inner.Abort();
+            }
+            public override void Abort(ConnectionAbortedException abortReason)
+            {
+                _inner.Abort(abortReason);
+            }
+
+            public override CancellationToken ConnectionClosed { get => _inner.ConnectionClosed; set => _inner.ConnectionClosed = value; }
+
+            public override string ConnectionId { get => _inner.ConnectionId; set => _inner.ConnectionId = value; }
+
+            public override async ValueTask DisposeAsync()
+            {
+                await _loggingPipe.DisposeAsync();
+                await _inner.DisposeAsync();
+            }
+
+            public override IFeatureCollection Features => _inner.Features;
+
+            public override IDictionary<object, object?> Items { get => _inner.Items; set => _inner.Items = value; }
+
+            public override EndPoint? LocalEndPoint { get => _inner.LocalEndPoint; set => _inner.LocalEndPoint = value; }
+            public override EndPoint? RemoteEndPoint { get => _inner.RemoteEndPoint; set => _inner.RemoteEndPoint = value; }
+
+            public override IDuplexPipe Transport { get => _inner.Transport; set => _inner.Transport = value; }
         }
 
         private class LoggingDuplexPipe : DuplexPipeStreamAdapter<LoggingStream>
